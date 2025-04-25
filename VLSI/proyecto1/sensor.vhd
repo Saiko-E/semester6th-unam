e29c38bf5486f027645d66211cd7d7f5d831f8f8
Eri <20eri.tq@gmail.com>
2025-04-24 18:06:13 -0600

    insertar datos

--- VLSI/proyecto1/sensor.vhd
+++ VLSI/proyecto1/sensor.vhd
@@ -6,33 +6,31 @@ use ieee.std_logic_unsigned.all;
 entity sensor is
 port	(clk, reset, echo: in std_logic; --señales de entrada para sensor
 		controlServo: out std_logic; --salida para servo
-		disparo: out std_logic; --salida para sensor
-		ssU, ssD: out std_logic_vector(6 downto 0) --Displays para distancia
+		b: out std_logic; --salida para sensor
+		unidades, decenas: out std_logic_vector(6 downto 0) --Displays para distancia
 		);
 end entity;
 
 architecture a of sensor is
-	signal clkSensor, clkServo, PWMSensor, trig: std_logic;
-	signal posIni, posFin: std_logic:='0';
+	signal clkl1, clkl2, pwm, trig: std_logic;
 	signal distancia, uni, dec: integer;
-	signal dutyServo: integer range 0 to 200:=85;
+	signal duty: integer range 0 to 200:=85;
 	
 begin
 	--servo
-	relojServo: entity work.divf(arqdivf) generic map(500) port map(clk, clkServo); --Reloj para servomotor, 50KHz
-	relojSensor: entity work.divf(arqdivf) generic map(25) port map(clk, clkSensor); --1MHZ - 1us
+	u1: entity work.divf(arqdivf) generic map(500) port map(clk, clkl2); --Reloj para servomotor, 50KHz
+	u2: entity work.divf(arqdivf) generic map(25) port map(clk, clkl1); --1MHZ - 1us
 	
-	senalPWM: entity work.senal(arqsenal) port map(clk, 25000000, 500, PWMSensor); --generacion de pwm para sensor
+	senal: entity work.senal(arqsenal) port map(clk, 25000000, 500, pwm); --generacion de pwm para sensor
 	
-	gatillo: entity work.trigger(arqtrigger) port map(PWMSensor, reset, echo, trig); --disparo sensor
-	disparo<=trig;
-	medicion: entity work.contador(arqConta) port map(echo, clkSensor, trig, distancia, uni, dec); --distancia sensor
+	a: entity work.trigger(arqtrigger) port map(pwm, reset, echo, trig); --disparo sensor
+	b<=trig;
+	medicion: entity work.contador(arqConta) port map(echo, clkl1, trig, distancia, uni, dec); --distancia sensor
 	
-	bcdUni: entity work.dec7seg(arqdec) port map(uni, ssU); --display unidades
-	bcdDec: entity work.dec7seg(arqdec) port map(dec, ssD); --display decimas
+	unidad: entity work.dec7seg(arqdec) port map(uni, unidades); --display unidades
+	decena: entity work.dec7seg(arqdec) port map(dec, decenas); --display decimas
 	
-	posiServo: entity work.movimiento(arqMov) port map(distancia, reset, dutyServo); --posiciones del servo
-	
-	pwmServo: entity work.senal(arqsenal) port map(clkServo, 1000, dutyServo, controlServo); --control del servo
+	posicion: entity work.movimiento(arqMov) port map(distancia, reset, duty); --posiciones del servo
+	servo: entity work.senal(arqsenal) port map(clkl2, 1000, duty, controlServo); --control del servo
 	
 end architecture;
